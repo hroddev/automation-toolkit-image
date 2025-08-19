6018d596c956fcd268553fc395fe678e167d12d2
Hector Rodriguez <hector.rodriguez@holamoda.net>
2025-08-19 15:00:59 +0000

    Remove obsolete PowerShell script for SharePoint Online user management

--- ps.ps1
+++
@@ -1,7 +0,0 @@
-# Conectar al servicio de SharePoint Online
-Connect-SPOService -Url https://holamoda-admin.sharepoint.com
-
-# Asignar permisos al nuevo usuario
-Set-SPOUser -Site "https://holamodapanama-my.sharepoint.com/personal/johana_robles_holamoda_net/_layouts/15/onedrive.aspx?view=0" `
-  -LoginName "demo.user@holamoda.net" `
-  -IsSiteCollectionAdmin $true
